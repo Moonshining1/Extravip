5048758d7e00e60554d39cf143c8a1b372a098d5
𝆺꯭𝅥⏤⵿⵿꯭꤫꤫︭ꭙ‌³⛦⃕͜💫🇲σ᭡፝֟ɳ🌙─‌⃛•𝆺꯭𝅥༎𓆪 <160732506+Moonshining1@users.noreply.github.com>
2024-09-26 23:26:06 +0530

    Update couple.py

--- plugins/couple.py
+++ plugins/couple.py
@@ -3,58 +3,35 @@ import pytz
 import os
 import random
 from pyrogram import filters
-from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup
+from pyrogram.types import InputMediaAnimation
 from pyrogram.enums import ChatType
 from telegraph import upload_file
-from PIL import Image, ImageDraw
 import requests
 
 from utils import get_image, get_couple, save_couple
 from VIPMUSIC import app
 
-
-# get current date in GMT+5:30 timezone
+# Get current date in GMT+5:30 timezone
 def get_today_date():
     timezone = pytz.timezone("Asia/Kolkata")
     now = datetime.now(timezone)
     return now.strftime("%d/%m/%Y")
 
-
-# get tomorrow's date in GMT+5:30 timezone
-
-
-def get_todmorrow_date():
+# Get tomorrow's date in GMT+5:30 timezone
+def get_tomorrow_date():
     timezone = pytz.timezone("Asia/Kolkata")
     tomorrow = datetime.now(timezone) + timedelta(days=1)
     return tomorrow.strftime("%d/%m/%Y")
 
-
-# Download image from URL
-
-
-def download_image(url, path):
-    response = requests.get(url)
-    if response.status_code == 200:
-        with open(path, "wb") as f:
-            f.write(response.content)
-    return path
-
-
 # Dates
-tomorrow = get_todmorrow_date()
+tomorrow = get_tomorrow_date()
 today = get_today_date()
-
 
 @app.on_message(filters.command(["couple", "couples"]))
 async def ctest(_, message):
     cid = message.chat.id
     if message.chat.type == ChatType.PRIVATE:
         return await message.reply_text("Tʜɪs ᴄᴏᴍᴍᴀɴᴅ ᴏɴʟʏ ᴡᴏʀᴋs ɪɴ ɢʀᴏᴜᴘs.")
-
-    p1_path = "downloads/pfp.png"
-    p2_path = "downloads/pfp1.png"
-    test_image_path = f"downloads/test_{cid}.png"
-    cppic_path = "downloads/cppic.png"
 
     try:
         is_selected = await get_couple(cid, today)
@@ -66,90 +43,40 @@ async def ctest(_, message):
                 if not i.user.is_bot and not i.user.is_deleted:
                     list_of_users.append(i.user.id)
 
+            if len(list_of_users) < 2:
+                return await msg.edit("Not enough users to form a couple.")
+
             c1_id = random.choice(list_of_users)
             c2_id = random.choice(list_of_users)
             while c1_id == c2_id:
-                c1_id = random.choice(list_of_users)
-
-            photo1 = (await app.get_chat(c1_id)).photo
-            photo2 = (await app.get_chat(c2_id)).photo
+                c2_id = random.choice(list_of_users)  # Fixing the loop to avoid selecting the same user
 
             N1 = (await app.get_users(c1_id)).mention
             N2 = (await app.get_users(c2_id)).mention
 
-            try:
-                p1 = await app.download_media(photo1.big_file_id, file_name=p1_path)
-            except Exception:
-                p1 = download_image(
-                    "https://telegra.ph/file/05aa686cf52fc666184bf.jpg", p1_path
-                )
-            try:
-                p2 = await app.download_media(photo2.big_file_id, file_name=p2_path)
-            except Exception:
-                p2 = download_image(
-                    "https://telegra.ph/file/05aa686cf52fc666184bf.jpg", p2_path
-                )
-
-            img1 = Image.open(p1)
-            img2 = Image.open(p2)
-
-            background_image_path = download_image(
-                "https://telegra.ph/file/96f36504f149e5680741a.jpg", cppic_path
-            )
-            img = Image.open(background_image_path)
-
-            img1 = img1.resize((437, 437))
-            img2 = img2.resize((437, 437))
-
-            mask = Image.new("L", img1.size, 0)
-            draw = ImageDraw.Draw(mask)
-            draw.ellipse((0, 0) + img1.size, fill=255)
-
-            mask1 = Image.new("L", img2.size, 0)
-            draw = ImageDraw.Draw(mask1)
-            draw.ellipse((0, 0) + img2.size, fill=255)
-
-            img1.putalpha(mask)
-            img2.putalpha(mask1)
-
-            draw = ImageDraw.Draw(img)
-
-            img.paste(img1, (116, 160), img1)
-            img.paste(img2, (789, 160), img2)
-
-            img.save(test_image_path)
-
+            # Prepare the message text
             TXT = f"""
-**Tᴏᴅᴀʏ's ᴄᴏᴜᴘʟᴇ ᴏғ ᴛʜᴇ ᴅᴀʏ:
+**Couple of the day 🎉:
 
-{N1} + {N2} = 💚
+{N1} + {N2} = ❣️
 
-Nᴇxᴛ ᴄᴏᴜᴘʟᴇs ᴡɪʟʟ ʙᴇ sᴇʟᴇᴄᴛᴇᴅ ᴏɴ {tomorrow}!!**
+New couple of the day can be chosen on {tomorrow}!!**
             """
 
-            await message.reply_photo(
-                test_image_path,
-                caption=TXT,
-                reply_markup=InlineKeyboardMarkup(
-                    [
-                        [
-                            InlineKeyboardButton(
-                                text="˹🕸️ ᴛᴧᴘ тᴏ sᴇᴇ ᴍᴧɢɪᴄ 🕸️˼",
-                                url=f"https://t.me/+fPsCUlG964E5MzY1",
-                            )
-                        ]
-                    ]
-                ),
-            )
-
+            # Delete the initial message
             await msg.delete()
-            a = upload_file(test_image_path)
-            for x in a:
-                img_url = "https://graph.org/" + x
-                couple = {"c1_id": c1_id, "c2_id": c2_id}
-                await save_couple(cid, today, couple, img_url)
+
+            # Specify the GIF URL
+            gif_url = "https://giphy.com/gifs/3xVLUygjT4AIxtqjS6"  # Replace with your GIF URL
+            
+            # Send the GIF with the message
+            await message.reply_animation(animation=gif_url, caption=TXT)
+
+            couple = {"c1_id": c1_id, "c2_id": c2_id}
+            await save_couple(cid, today, couple)
 
         else:
+            # Handle the case when a couple has already been selected
             msg = await message.reply_text("❣️")
             b = await get_image(cid)
             c1_id = int(is_selected["c1_id"])
@@ -158,35 +85,22 @@ Nᴇxᴛ ᴄᴏᴜᴘʟᴇs ᴡɪʟʟ ʙᴇ sᴇʟᴇᴄᴛᴇᴅ ᴏɴ {tomorro
             c2_name = (await app.get_users(c2_id)).first_name
 
             TXT = f"""
-**Tᴏᴅᴀʏ's ᴄᴏᴜᴘʟᴇ ᴏғ ᴛʜᴇ ᴅᴀʏ 🎉:
+**Couple of the day 🎉:
 
 [{c1_name}](tg://openmessage?user_id={c1_id}) + [{c2_name}](tg://openmessage?user_id={c2_id}) = ❣️
 
-Nᴇxᴛ ᴄᴏᴜᴘʟᴇs ᴡɪʟʟ ʙᴇ sᴇʟᴇᴄᴛᴇᴅ ᴏɴ {tomorrow}!!**
+New couple of the day can be chosen on {tomorrow}!!**
             """
-            await message.reply_photo(
-                b,
-                caption=TXT,
-                reply_markup=InlineKeyboardMarkup(
-                    [
-                        [
-                            InlineKeyboardButton(
-                                text="˹🕸️ ᴛᴧᴘ тᴏ sᴇᴇ ᴍᴧɢɪᴄ 🕸️˼",
-                                url=f"https://t.me/+fPsCUlG964E5MzY1",
-                            )
-                        ]
-                    ]
-                ),
-            )
+
+            # Specify the GIF URL
+            gif_url = "https://giphy.com/gifs/3xVLUygjT4AIxtqjS6"  # Replace with your GIF URL
+            
+            # Delete the initial message and send the GIF with the message
             await msg.delete()
+            await message.reply_animation(animation=gif_url, caption=TXT)
 
     except Exception as e:
         print(str(e))
     finally:
-        try:
-            os.remove(p1_path)
-            os.remove(p2_path)
-            os.remove(test_image_path)
-            os.remove(cppic_path)
-        except Exception as cleanup_error:
-            print(f"Error during cleanup: {cleanup_error}")
+        # Cleanup code (if any files are created)
+        pass
